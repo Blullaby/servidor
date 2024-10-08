fc1899ecfcd155c1989e0aa97deb837bea56355b
Blullaby <164385799+Blullaby@users.noreply.github.com>
2024-10-07 19:39:14 -0500

    Add files via upload

---
+++ servidor_web/js/scripts.js
@@ -0,0 +1,23 @@
+function showFeatures() {
+    document.getElementById("info").innerHTML = `
+    <h3>Smartphone X100 Features:</h3>
+    <ul>
+        <li>6.7-inch OLED Display</li>
+        <li>Quad-camera system</li>
+        <li>5G Connectivity</li>
+        <li>5000mAh Battery</li>
+        <li>128GB/256GB Storage</li>
+    </ul>`;
+}
+
+function showPrice() {
+    document.getElementById("info").innerHTML = `
+    <h3>Price:</h3>
+    <p>The starting price for Smartphone X100 is $999.</p>`;
+}
+
+function buyNow() {
+    document.getElementById("info").innerHTML = `
+    <h3>Buy Now:</h3>
+    <p>You can purchase the Smartphone X100 from our official website.</p>`;
+}
